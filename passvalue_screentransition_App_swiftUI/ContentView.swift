f1677cc065b77a565b8ea5fee138670172f9bda1
hiroyuki0402 <hiro1990.cloud@gmail.comcd>
2023-09-29 21:04:18 +0900

    Initial Commit

---
+++ passvalue_screentransition_App_swiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  passvalue_screentransition_App_swiftUI
+//
+//  Created by SHIRAISHI HIROYUKI on 2023/09/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ passvalue_screentransition_App_swiftUI/passvalue_screentransition_App_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  passvalue_screentransition_App_swiftUIApp.swift
+//  passvalue_screentransition_App_swiftUI
+//
+//  Created by SHIRAISHI HIROYUKI on 2023/09/29.
+//
+
+import SwiftUI
+
+@main
+struct passvalue_screentransition_App_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
